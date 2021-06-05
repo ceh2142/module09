14f1395b90a31ca1b7fc751e218ce0737128e639
ceh2142 <chantelle.hudspeth@bellevuecollege.edu>
2021-06-04 18:06:33 -0700

    Update dots.js

--- dots.js
+++ dots.js
@@ -1,29 +1,6 @@
-var mybutton = document.getElementById("clickme");
-  mybutton.addEventListener("click", function() {
-    alert("You clicked!");
-  });
-
-  var button = document.getElementById(“onlyOnce");
-  function once() {
-    console.log("Done.");
-    button.removeEventListener("click", once);
-  }
-  button.addEventListener("click", once);
-
-
-var element = document.getElementsByTagName("div");
-for (index = element.length - 1; index >= 0; index--) {
-    element[index].parentNode.removeChild(element[index]);
-}
-
-// Let us stop the propagation of events
-
-event.stopPropagation();
-  });
-  addEventListener("click", function(event) {
-    var dot = document.createElement("div");
-    dot.className = "dot";
-    dot.style.left = (event.pageX - 4) + "px";
-    dot.style.top = (event.pageY - 4) + "px";
-    document.body.appendChild(dot);
-  });
+var dot = document.getElementById("dot");
+       addEventListener('click', function(event){
+          dot.style.visibility="visible";
+          box.left = event.pageX;
+          box.top = event.pageY;
+   });

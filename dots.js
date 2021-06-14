bd0246415cdd6dc3ce1cb4b8cd0d38e04de6f30e
ceh2142 <chantelle.hudspeth@bellevuecollege.edu>
2021-06-14 16:53:16 -0700

    js file changed

--- dots.js
+++ dots.js
@@ -17,15 +17,3 @@ for (index = element.length - 1; index >= 0; index--) {
     element[index].parentNode.removeChild(element[index]);
 }
 
-// Let us stop the propagation of events
-
-event.stopPropagation();
-  });
-  addEventListener("click", function(event) {
-    var dot = document.createElement("dot");
-    dot.className = "dot";
-    dot.style.left = (event.pageX - 4) + "px";
-    dot.style.top = (event.pageY - 4) + "px";
-    document.body.appendChild(dot);
-  });
-

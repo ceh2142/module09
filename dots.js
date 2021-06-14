762b1c96857f80ba6700120eb5e0c30d9e5f814e
ceh2142 <chantelle.hudspeth@bellevuecollege.edu>
2021-06-14 16:55:32 -0700

    Update dots.js

--- dots.js
+++ dots.js
@@ -17,3 +17,12 @@ for (index = element.length - 1; index >= 0; index--) {
     element[index].parentNode.removeChild(element[index]);
 }
 
+event.stopPropagation();
+  });
+  addEventListener("click", function(event) {
+    var dot = document.createElement("div");
+    dot.className = "dot";
+    dot.style.left = (event.pageX - 4) + "px";
+    dot.style.top = (event.pageY - 4) + "px";
+    document.body.appendChild(dot);
+  });

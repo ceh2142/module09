c7a1fdd821f4846b05e804db8e9e9c6c552e94a4
ceh2142 <chantelle.hudspeth@bellevuecollege.edu>
2021-06-15 17:09:46 -0700

    Update dots.js

--- dots.js
+++ dots.js
@@ -1,13 +1,3 @@
-addEventListener('click', createDot);
-
-function createDot(event) {
-  var dot = document.createElement('div');
-  dot.className = 'dot';
-  dot.style.left = event.pageX + 'px';
-  dot.style.top = event.pageY + 'px';
-  document.body.appendChild(dot);
-}
-
 var mybutton = document.querySelector("button");
 mybutton.addEventListener("click", function(event) {
 
